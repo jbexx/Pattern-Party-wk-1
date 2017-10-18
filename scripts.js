70fdc3db035f78dbd7b3cd8590088bce88338a94
Jonathan Beckman <jbecks.media@gmail.com>
2017-10-18 16:09:54 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,8 @@
-const runTabs = (e) => {
-    let currentTab = $(e.target).attr('data-id');
-
+$('.nav-item').on('click', (e) => {
     $('.nav-item').removeClass('is-active');
     $('.plus').text('+');
     $('.content-container').addClass('is-hidden');
     $(e.target).addClass('is-active');
     $(e.target).find('.plus').text('-')
-    $("#" + currentTab).removeClass('is-hidden');
-}
-
-$('.nav-item').on('click', (e) => {runTabs(e)})
+    $("#" + $(e.target).attr('data-id')).removeClass('is-hidden');
+});
