97365f21afae23f5e625584cbc6da8e72fa3ac3a
Jonathan Beckman <jbecks.media@gmail.com>
2017-11-01 14:28:07 -0600

    add nav bar

--- scripts.js
+++ scripts.js
@@ -6,3 +6,17 @@ $('.nav-item').on('click', (e) => {
     $(e.target).find('.plus').text('-')
     $("#" + $(e.target).attr('data-id')).removeClass('is-hidden');
 });
+
+$('.title').on('click', () => {
+    if ( $('.mobile-navs').css('display') === 'none' ) {
+        $('.mobile-navs').css('display', 'flex')
+    } else {
+        $('.mobile-navs').css('display', 'none')  
+    }
+});
+
+$(window).on('resize', () => {
+    if ( $(window).width() > 720) {
+        $('.mobile-navs').css('display', 'none')        
+    }
+});
